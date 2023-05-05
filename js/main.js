647d1268c8914d6ccb7a952886992789c3981c26
Alejandro Arroba <Alex23arroba@gmail.com>
2023-05-05 19:10:56 +0200

    arreglado el login modificando index.html styles.css y main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$('.toggle').click(function(){
+    $('.formulario').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    });
+}); 
+
 const username = document.getElementById('username')
 const password = document.getElementById('password')
 const button = document.getElementById('button')
