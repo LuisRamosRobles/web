76a5c72bd5438b4e3df590d7e48ab0a2eeb67bed
Xenom22gtf <Alex23arroba@gmail.com>
2023-05-25 12:27:01 +0200

    Control de parametros en contact
    Se ha añadido controles para el nombre, que sean máximo 20 caracteres y que no puedas añadir solo espacios, en el número que tengas que añadir si o si 9 dígitos y que no puedas añadir otra cosa que no sean números y en el correo que sea del formato requerido

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+function validarNombre(input) {
+    if (input.validity.valueMissing) {
+        input.setCustomValidity('Por favor, ingrese su nombre completo.');
+    } else if (/^\s+$/.test(input.value)) {
+        input.setCustomValidity('Ingresa un nombre valido.');
+    } else {
+        input.setCustomValidity('');
+    }
+}
+
+
+
+    function validarTlfn(input) {
+    // Remueve todos los caracteres que no sean números
+    input.value = input.value.replace(/\D/g, '');
+
+    // Limita la longitud del valor a 9 caracteres
+    if (input.value.length < 9) {
+        input.setCustomValidity('Ingrese el numero completo de 9 digito.');
+    } else {
+        input.setCustomValidity('');
+    }
+}
+
